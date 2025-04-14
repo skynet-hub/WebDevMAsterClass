5c0a7748cdbad1483d7da7df07ece24a88d9fb7e
Magobo <malesaojhb024@student.wethinkcode.co.za>
2025-04-14 15:56:35 +0200

    Playing with prototype in JS

---
+++ JSOOP/PlayingPrototypes.js
@@ -0,0 +1,9 @@
+let Computer = {CPU: 12} //12 represents threads
+
+let lenovo = {
+    screen: "HD",
+}
+
+Object.setPrototypeOf(lenovo, Computer)
+
+console.log(Object.getOwnPropertyNames(lenovo))
